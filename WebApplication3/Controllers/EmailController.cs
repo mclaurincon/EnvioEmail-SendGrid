574785098db7efa216ac4b0cf15adad28103f413
Maria Claudia Rincon <100317526+mclaurincon@users.noreply.github.com>
2023-08-21 17:30:09 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication3/Controllers/EmailController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SendGrid.Helpers.Mail;
+using SendGrid;
+
+namespace WebApplication3.Controllers
+{
+    public class EmailController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task<ActionResult> EnviarEmail()
+        {
+            var apiKey = _configuration["AppSettings:SendGridApiKey"];
+            var client = new SendGridClient(apiKey);
+
+            var from = new EmailAddress("claudiarincon@uol.com.br", "Claudia");
+            var to = new EmailAddress("clauzinhando@gmail.com", "Clauzinhando");
+            var assunto = "Assunto do Email";
+            var mensagem = "Conteúdo do Email";
+
+            var msg = MailHelper.CreateSingleEmail(from, to, assunto, mensagem, mensagem);
+            var response = await client.SendEmailAsync(msg);
+            Console.WriteLine($"Enviado");
+            return View();
+        }
+    }
+}
